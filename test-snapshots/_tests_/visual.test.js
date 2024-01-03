af3bb541f3d99e4b25721b25d69f9a8abf8297ed
Fritzie1860 <primanandafritzie@gmail.com>
2024-01-03 11:48:22 +0800

    single emelent snapshot

--- test-snapshots/_tests_/visual.test.js
+++ test-snapshots/_tests_/visual.test.js
@@ -34,4 +34,16 @@ describe('Visual Regression Testing',()=>{
 
         )
     })
+
+    test('Single Element Snapshot', async function(){
+        await page.goto('https://www.example.com')
+        const h1 = await page.waitForSelector('h1') 
+        // instead of targeting the page, we are now targeting the h1 and it will only screenshot the h1 area
+        const image = await h1.screenshot()
+        // if the percentage of the different pixels is more than 0.01% it will throw an error
+        expect(image).toMatchImageSnapshot({
+            failureThresholdType: 'percent',
+            failureThreshold: 0.01
+        })
+    })
 })
